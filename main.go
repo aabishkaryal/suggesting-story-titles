59515087feee012f7d4caf37cbe7e95b92788d8e
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-03-05 17:16:08 +0545

    Added comments to main function

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
-const NUM_LABELS = 5
+// Show the top 3 most popular Labels
+// Could be more if multiple labels share the same position
+const NUM_LABELS = 3
+
+// Number of workers/goroutines
 const NUM_WORKER = 8
 
 func main() {
@@ -23,20 +27,24 @@ func main() {
 		fileName = os.Args[1]
 	}
 
+	// Load env variables with godotenv
 	err := godotenv.Load()
 	utils.HandleError(err, "Error loading environment variables")
 
+	// Initialize the map client
 	utils.InitializeMapClient(os.Getenv("API_KEY"))
 
+	// Open and read the file
 	file, err := os.Open(fileName)
 	utils.HandleError(err, "Error opening file")
-
 	records, err := csv.NewReader(file).ReadAll()
 	utils.HandleError(err, "Error reading file")
 
+	// Create channels for concurrency
 	jobs := make(chan []string, len(records))
 	results := make(chan []string, len(records))
 
+	// Spawn workers
 	for i := 0; i < NUM_WORKER; i++ {
 		go workerpool.Worker(jobs, results)
 	}
@@ -46,6 +54,7 @@ func main() {
 	}
 	close(jobs)
 
+	// Keep track of how many times a label was recommended
 	labelCount := make(map[string]int)
 	for range records {
 		recLabels := <-results
@@ -54,6 +63,7 @@ func main() {
 		}
 	}
 
+	// Print all labels if number of all labels is lower than how many we want
 	if len(labelCount) <= NUM_LABELS {
 		for k := range labelCount {
 			fmt.Println(k)
@@ -61,16 +71,18 @@ func main() {
 		return
 	}
 
+	// sort the count to decide the threshold to ignore labels
 	counts := make(sort.IntSlice, 0, len(labelCount))
 	for _, v := range labelCount {
 		counts = append(counts, v)
 	}
-
 	counts.Sort()
 	for i, j := 0, len(counts)-1; i < j; i, j = i+1, j-1 {
 		counts[i], counts[j] = counts[j], counts[i]
 	}
+
 	threshold := counts[NUM_LABELS]
+	// Only show labels with recommendation above the threshold
 	for k, v := range labelCount {
 		if v >= threshold {
 			fmt.Println(k)
