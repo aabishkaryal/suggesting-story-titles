5dc4460134847e3968ba69562cd9b230bf37028a
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-03-05 16:00:07 +0545

    - using a default fileName in case fileName isn't provided by user - fixedBug load env at start - abstract LabelRecord function

--- main.go
+++ main.go
@@ -3,37 +3,35 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"log"
 	"os"
-	"strconv"
 
+	"github.com/aabishkaryal/suggesting-story-titles/labels"
 	"github.com/aabishkaryal/suggesting-story-titles/utils"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	var fileName string
 	if len(os.Args) < 2 {
-		log.Fatalln("Please provide a csv file to read data from.")
+		// log.Fatalln("Please provide a csv file to read data from.")
+		fileName = "./data/0.csv"
+	} else {
+		fileName = os.Args[1]
 	}
-	file, err := os.Open(os.Args[1])
+
+	err := godotenv.Load()
+	utils.HandleError(err, "Error loading environment variables")
+
+	utils.InitializeMapClient(os.Getenv("API_KEY"))
+
+	file, err := os.Open(fileName)
 	utils.HandleError(err, "Error opening file")
 
 	records, err := csv.NewReader(file).ReadAll()
 	utils.HandleError(err, "Error reading file")
 
-	err = godotenv.Load()
-	utils.HandleError(err, "Error loading environment variables")
-
 	for _, rec := range records {
-		fmt.Println(labelRecord(rec))
+		fmt.Println(labels.LabelRecord(rec))
 	}
 
-}
-
-func labelRecord(record []string) string {
-	lat, _ := strconv.ParseFloat(record[1], 32)
-	lng, _ := strconv.ParseFloat(record[2], 32)
-	date := utils.ParseDate(record[0])
-	address := utils.ReverseGeocode(lat, lng)
-	return date.Month().String() + "\t" + address
 }
